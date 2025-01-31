36691b80cc50498b712dea5df0bcf8eeda075968
vichedanarith <vichedanarith2027@u.northwestern.edu>
2025-01-30 23:29:08 -0600

    edited common blocks category

--- src/toolbox.js
+++ src/toolbox.js
@@ -11,61 +11,73 @@ You probably don't need every single block, and should consider either rewriting
 your toolbox from scratch, or carefully choosing whether you need each block
 listed here.
 */
+
 export const toolbox = {
-  kind: 'categoryToolbox', // Keeps categories
-  contents: [
+  "kind": "categoryToolbox",
+  "contents": [
     {
-      kind: 'category',
-      name: 'Turtles',
-      categorystyle: 'netlogo_category',
-      contents: [
-        { kind: 'block', type: 'setup_block' },
-        { kind: 'block', type: 'go_block' },
-        { kind: 'block', type: 'ask_block' },
-        { kind: 'block', type: 'if_block' },
-      ],
+      "kind": "category",
+      "name": "NetLogo",
+      "colour": "#5CA65C",
+      "contents": [
+        { "kind": "block", "type": "setup_block" },
+        { "kind": "block", "type": "go_block" },
+        { "kind": "block", "type": "ask_block" },
+        { "kind": "block", "type": "if_block" },
+        { "kind": "block", "type": "move_block" }
+      ]
     },
     {
-      kind: 'category',
-      name: 'Patches',
-      categorystyle: 'netlogo_category',
-      contents: [
-        { kind: 'block', type: 'setup_block' },
-      ],
+      "kind": "category",
+      "name": "Logic",
+      "colour": "#5C81A6",
+      "contents": [
+        { "kind": "block", "type": "controls_if" },
+        { "kind": "block", "type": "logic_compare" },
+        { "kind": "block", "type": "logic_operation" },
+        { "kind": "block", "type": "logic_negate" },
+        { "kind": "block", "type": "logic_boolean" }
+      ]
     },
     {
-      kind: 'category',
-      name: 'Events',
-      categorystyle: 'netlogo_category',
-      contents: [
-        { kind: 'block', type: 'setup_block' },
-      ],
+      "kind": "category",
+      "name": "Loops",
+      "colour": "#5CA68D",
+      "contents": [
+        { "kind": "block", "type": "controls_repeat_ext" },
+        { "kind": "block", "type": "controls_whileUntil" },
+        { "kind": "block", "type": "controls_for" }
+      ]
     },
     {
-      kind: 'category',
-      name: 'Controls',
-      categorystyle: 'netlogo_category',
-      contents: [
-        { kind: 'block', type: 'setup_block' },
-      ],
+      "kind": "category",
+      "name": "Math",
+      "colour": "#5C68A6",
+      "contents": [
+        { "kind": "block", "type": "math_number" },
+        { "kind": "block", "type": "math_arithmetic" },
+        { "kind": "block", "type": "math_single" },
+        { "kind": "block", "type": "math_trig" },
+        { "kind": "block", "type": "math_round" }
+      ]
     },
     {
-      kind: 'category',
-      name: 'Maths',
-      categorystyle: 'netlogo_category',
-      contents: [
-        { kind: 'block', type: 'setup_block' },
-      ],
+      "kind": "category",
+      "name": "Text",
+      "colour": "#A65C81",
+      "contents": [
+        { "kind": "block", "type": "text" },
+        { "kind": "block", "type": "text_length" },
+        { "kind": "block", "type": "text_join" }
+      ]
     },
     {
-      kind: 'category',
-      name: 'Variables',
-      categorystyle: 'netlogo_category',
-      contents: [
-        { kind: 'block', type: 'setup_block' },
-      ],
+      "kind": "category",
+      "name": "Variables",
+      "custom": "VARIABLE",
+      "colour": "#A65C5C"
     },
-  ],
+  ]
 };
 
 // export const toolbox = {
